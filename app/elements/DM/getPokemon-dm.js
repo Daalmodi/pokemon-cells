9dca6835c67723c467f8eb7152aaeb38893261a6
Daniel Moya <daalmodi@gmail.com>
2024-10-21 18:27:56 -0500

    Card Template

---
+++ app/elements/DM/getPokemon-dm.js
@@ -0,0 +1,9 @@
+export const getPokemon = () =>{
+  for (let index = 1; index <= 20; index++) {
+
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
+      .then((response)=> response.json())
+      .then((data)=>this.renderPokemons(data));
+  }
+};
--- app/elements/UI/list-pokemon.js
+++ app/elements/UI/list-pokemon.js
@@ -1,5 +1,5 @@
 import { html, LitElement } from 'lit-element';
-
+import '@bbva-web-components/bbva-web-panel-outstanding-opportunity/bbva-web-panel-outstanding-opportunity.js';
 export class GetListPokemon extends LitElement {
 
   static get is() {
@@ -16,7 +16,7 @@ export class GetListPokemon extends LitElement {
 
   makeRequest() {
 
-    for (let index = 1; index <= 20; index++) {
+    for (let index = 1; index <= 31; index++) {
 
 
       fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
@@ -33,19 +33,20 @@ export class GetListPokemon extends LitElement {
 
   render() {
     return html`
-        <div>
-          ${this.pokemones.map((pokemon) => html
-  `<h3>${pokemon.name}</h3>
-  <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-  <p>Tipos: </p>
     <div>
-      ${pokemon.types.map(type => html`
-        <li>${type.type.name}</li>
+      ${this.pokemones.map((pokemon) => html`
+        <bbva-web-panel-outstanding-opportunity-item
+          heading=${pokemon.name}
+          heading-icon=""
+          link="Link"
+          slot="main"
+          bg-img=${pokemon.sprites.other.dream_world.front_default}
+        >
+           Tipos : ${pokemon.types.map((type) => html` ${type.type.name} `)}
+        </bbva-web-panel-outstanding-opportunity-item>
       `)}
     </div>
-
-          `)}
-        </div>`;
+  `;
   }
 
 }
